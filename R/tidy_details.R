f6902342444435749e9e1b6094a926211071a38a
Dries Moreels <dries.moreels@ugent.be>
2015-03-25 21:24:47 +0100

    turned into function (prevent package build errors)

--- R/tidy_details.R
+++ R/tidy_details.R
@@ -1,45 +1,53 @@
-# small codeblock
-# appends detail_memento.R
-# 
-# response from Timetravel JSON API is a list of differently structured lists
-# turn it into tidy data frame
+#' Memento details converted to tidy dataframe
+#' 
+#'  Response from Timetravel JSON API is a list of differently structured lists
+#' 
+#'  Shows the memento for a given resource with an archival/version datetime 
+#'  that is close to a desired one, as a dataframe.
+#'  
+#'  @param uri desired uri of the web resource
+#'  @param date_time date and time of the desired archived version
+#'  
+#'  @examples \dontrun{
+#'  tidy_details("http://cnn.com", "2001-09-11")
+#'  }
+#'  @export
 
-## expect variable temp with results from detail_momento()
+tidy_details <- function(uri = NULL, date_time = NULL) {
+  if(is.null(uri)) 
+    stop ("Please provide a uri")
+  if(is.null(date_time)) 
+    stop("Please provide a date, eg. 2011-01-02")
 
-library(plyr)
-library(lubridate)
+  # get memento
+  temp <- detail_memento(uri = uri, date_time = date_time)
 
-herenow <- here()
+  library(plyr)
+  library(lubridate)
+  herenow <- here()
 
-# mementos are a group of lists with nested structure
-first <- ldply(temp$mementos, data.frame)
-colnames(first) <- c("rel", "datetime", "uri")
-## convert datetime to R time object
-first$datetime <- ymd_hms(first$datetime)
-
-# timegate URI is a simple string
-third <- cbind("timegate", herenow, ldply(temp$timegate_uri, data.frame))
-colnames(third) <- c("rel", "datetime", "uri")
-
-# timemap URI's are provided in two formats
-fourth <- cbind("timemap-json", herenow, ldply(temp$timemap_uri$json_format, data.frame))
-colnames(fourth) <- c("rel", "datetime", "uri")
-fifth <- cbind("timemap-link", herenow, ldply(temp$timemap_uri$link_format, data.frame))
-colnames(fifth) <- c("rel", "datetime", "uri")
-
-# all together
-result <- rbind(first, third, fourth, fifth)
-
-# original URI is a simple string
-
-## could be mapped as a row with NA as a timestamp
-## second <- cbind("original", NA, ldply(temp$original_uri, data.frame))
-## colnames(second) <- c("rel", "datetime", "uri")
-
-## should be mapped as a column with fixed content
-result <- cbind(result, rep(temp$original_uri, nrow(result)))
-colnames(result) <- c("rel", "datetime", "uri", "original")
-
-
-
-View(result)
+  # mementos are a group of lists with nested structure
+  first <- ldply(temp$mementos, data.frame)
+  colnames(first) <- c("rel", "datetime", "uri")
+  ## convert datetime to R time object
+  first$datetime <- ymd_hms(first$datetime)
+  
+  # timegate URI is a string
+  third <- cbind("timegate", herenow, ldply(temp$timegate_uri, data.frame))
+  colnames(third) <- c("rel", "datetime", "uri")
+  
+  # timemap URI's are provided in two formats
+  fourth <- cbind("timemap-json", herenow, ldply(temp$timemap_uri$json_format, data.frame))
+  colnames(fourth) <- c("rel", "datetime", "uri")
+  fifth <- cbind("timemap-link", herenow, ldply(temp$timemap_uri$link_format, data.frame))
+  colnames(fifth) <- c("rel", "datetime", "uri")
+  
+  # all together
+  result <- rbind(first, third, fourth, fifth)
+  
+  # repeat original URI as a column
+  result <- cbind(result, rep(temp$original_uri, nrow(result)))
+  colnames(result) <- c("rel", "datetime", "uri", "original")
+  
+  result
+}
